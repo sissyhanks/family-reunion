cb45fb1b1beddc9713eee81a59aacf91b578533a
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-20 11:04:08 -0500

    path helper

--- app.js
+++ app.js
@@ -18,8 +18,12 @@ app.use("/shop", shopRoutes);
 // this function will run becore every app instance
 // next will run when called and go on to next middlewar funcitron in the line (if you don't send response>>  will not go on to next middleware)
 
+const rootDir = require("./util/path");
+
 app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, "./", "views", "fourohfour.html"));
+  res.sendFile(path.join(rootDir, "views", "fourohfour.html"));
 });
 
 app.listen(3000);
+
+// module.exports = path.dirname(require.main.filename);
---
+++ util/path.js
@@ -0,0 +1,4 @@
+const req = require("express/lib/request");
+const path = require("path");
+
+module.exports = path.dirname(require.main.filename);
