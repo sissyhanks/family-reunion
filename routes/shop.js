14cb1f051570028daea8fcd30fa15caf42581b9f
Bernadette McKnight <sissyhanks@yahoo.com>
2022-01-20 10:46:15 -0500

    serve html files

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
+const path = require("path");
+
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  console.log("middle");
-  res.send("<h1>middle!!</h1>");
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
 
 module.exports = router;
