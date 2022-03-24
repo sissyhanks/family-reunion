6bdfab888860fbd13abbf9b8154d5522328b0c03
Bernadette McKnight <sissyhanks@yahoo.com>
2022-03-24 14:40:15 -0400

    Output dynamic content

--- routes/shop.js
+++ routes/shop.js
@@ -1,4 +1,5 @@
 // fok importing data
+// rendp rendering in pug
 const path = require("path");
 
 const express = require("express");
@@ -13,8 +14,17 @@ router.get("/", (req, res, next) => {
   // fok 2) grab data entered into form
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, "views", "shop.html"));
-  // in order to be rendered on pug use res.render instrad of send file & grab pug file ... anmed path des not need to be constructewd
-  res.render("shop");
+  // rendp 1) grab the data you want to render
+  const products = adminData.products;
+  // in order to be rendered on pug use res.render instead of send file & grab pug file ... also, path des not need to be constructed
+  // rendp 2) pass in the data  in as a second argument in render methods it will be available by the page that is rendering it
+
+  res.render("shop", {
+    // build object... docTitle is hard coded and prods pulls form the products array built from add-=product page
+    docTitle: "Shop",
+    prods: products,
+  });
 });
+// rendp 3) pass in the data as an object that will be use as a key name in rendering
 
 module.exports = router;
