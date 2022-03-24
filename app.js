f2542af6279033e5d5d4ef8373a878a3cf8a92c0
Bernadette McKnight <sissyhanks@yahoo.com>
2022-03-24 11:47:53 -0400

    install and inplement pug

--- app.js
+++ app.js
@@ -18,6 +18,12 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use("/shop", shopRoutes);
 
+//set global standard to use specific view engin
+// https://expressjs.com/en/api.html#app.set
+// telling what engine to use (no other config needed because pug is ready out of the box) and where to find views
+app.set("view engine", "pug");
+app.set("views", "views");
+
 // this function will run before every app instance
 // next will run when called and go on to next middleware function in the line (if you don't send response>>  will not go on to next middleware)
 
--- routes/shop.js
+++ routes/shop.js
@@ -11,8 +11,10 @@ const router = express.Router();
 
 router.get("/", (req, res, next) => {
   // fok 2) grab data entered into form
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // console.log(adminData.products);
+  // res.sendFile(path.join(rootDir, "views", "shop.html"));
+  // in order to be rendered on pug use res.render instrad of send file & grab pug file ... anmed path des not need to be constructewd
+  res.render("shop");
 });
 
 module.exports = router;
